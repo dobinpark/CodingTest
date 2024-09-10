27dee3ae5568a6a43ca4d9ef02423336ecfe17f6
Park-Gun <ff5026@naver.com>
2024-09-10 23:27:22 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 159 ms, Memory: 20,552 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String args[]) throws Exception	{
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for(int test_case = T; test_case >= 0; test_case--)	{
+            System.out.print(test_case + " ");
+        }
+    }
+}
