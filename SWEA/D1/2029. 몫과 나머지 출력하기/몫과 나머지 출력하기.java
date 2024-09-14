9a9ebad730575268393664f55fcab254750beceb
Park-Gun <ff5026@naver.com>
2024-09-14 20:51:00 +0900

    [D1] Title: 몫과 나머지 출력하기, Time: 188 ms, Memory: 20,992 KB -BaekjoonHub

---
+++ SWEA/D1/2029. 몫과 나머지 출력하기/몫과 나머지 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String args[])	{
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 1; i <= t; i++) {
+            int a = sc.nextInt(), b = sc.nextInt();
+            System.out.printf("#%d %d %d\n", i, a / b, a % b);
+        }
+    }
+}
